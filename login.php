5a7ddfc3e013d2ec5bb7996c753abfbc4d4c9865
TuanKietIT <ngokiet68@gmail.com>
2023-02-26 10:13:04 +0700

    them label va textbox matkhau

--- login.php
+++ login.php
@@ -1,9 +1,11 @@
-
 <html>
+
 <body>
     <form method="POST">
         <label for="">Usename:</label>
         <input type="text" name="txtUseName"><br><br>
+        <label for="">Mật khẩu:</label>
+        <input type="password" name="txtmatkhau"><br><br>
     </form>
 </body>
 
